c3135ba02fde960d5059297bae47cccdab1c7128
Dimitri V <dvardidze15@gmail.com>
2024-07-22 18:17:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById('container').style.display = "none";
+    document.getElementById('ty').style.display = "block";
+}
+function closePopup(){
+    document.getElementById('container').style.display = "block";
+    document.getElementById('ty').style.display = "none";
+}
